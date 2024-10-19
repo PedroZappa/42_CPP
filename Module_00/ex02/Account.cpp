2ea55233b5d163c3ded137139179374ae66959dd
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-10-19 09:45:10 +0100

    Module_00/ex02/Account.cpp : _displayTimestamp() implemented

--- Module_00/ex02/Account.cpp
+++ Module_00/ex02/Account.cpp
@@ -11,6 +11,8 @@
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <ctime>    // std::time
+#include <iomanip>  // std::setfill std::setw
 #include <iostream> // std::cout std::endl
 
 /**
@@ -36,4 +38,35 @@ Account::Account(int initial_deposit)
 	std::cout << "index:" << this->_accountIndex << ";";
 	std::cout << "amount:" << this->_amount << ";";
 	std::cout << "created" << std::endl;
+}
+
+/**
+ *	Account class Destructor
+ */
+Account::~Account(void)
+{
+	// Write Account's destruction log to std::cout
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "closed" << std::endl;
+}
+
+/**
+ * Account clkass Helper Methods
+ */
+static void _displayTimestamp(void)
+{
+	std::time_t	rawtime;
+	std::tm *timestamp;
+
+	rawtime = std::time(0);
+	timestamp = std::localtime(&rawtime);
+
+	std::cout << "[" << timestamp->tm_year + 1900;
+	std::cout << std::setfill('0') << std::setw(2) << timestamp->tm_mon;
+	std::cout << std::setfill('0') << std::setw(2) << timestamp->tm_mday << "_";
+	std::cout << std::setfill('0') << std::setw(2) << timestamp->tm_hour;
+	std::cout << std::setfill('0') << std::setw(2) << timestamp->tm_min;
+	std::cout << std::setfill('0') << std::setw(2) << timestamp->tm_sec << "] ";
 }
