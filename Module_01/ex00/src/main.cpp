35fcc4d91381d57fa4b361c25599f0e7c7aacfa4
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-10-19 11:55:01 +0100

    Module_01/ex00/main.cpp : noted

--- Module_01/ex00/src/main.cpp
+++ Module_01/ex00/src/main.cpp
@@ -6,9 +6,15 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 11:43:28 by passunca          #+#    #+#             */
-/*   Updated: 2024/10/19 11:43:42 by passunca         ###   ########.fr       */
+/*   Updated: 2024/10/19 11:54:27 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/* 
+** NOTE: In which case is it better to allocate the Zombie    
+**       on the stack or on the heap?
+** WARN: Zombies must be destroyed when not needed anymore
+*/
 
 int main(void) {
 	return (0);
