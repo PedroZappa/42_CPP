abcfcfb44239b2a151362bf584491ea0911b74b2
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-10-19 09:28:02 +0100

    Module_00/ex02/Account.cpp : Constructor implmented

--- Module_00/ex02/Account.cpp
+++ Module_00/ex02/Account.cpp
@@ -11,3 +11,29 @@
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream> // std::cout std::endl
+
+/**
+ *	Initializes Account's class static private members
+ */
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+/**
+ *	Account class Constructor
+ */
+Account::Account(int initial_deposit)
+{
+	this->_accountIndex = Account::_nbAccounts++;
+	this->_amount = initial_deposit;
+	this->_nbWithdrawals = 0;
+	this->_nbDeposits = 0;
+	Account::_totalAmount += initial_deposit;
+
+	// Write Account's creation log to std::cout
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "created" << std::endl;
+}
