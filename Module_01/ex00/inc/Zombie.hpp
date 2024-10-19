6c717d87bdf6ca652faa2f0bfdc77849cd6b2383
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-10-19 12:29:05 +0100

    Module_01/ex00/ : Zombie functions

--- Module_01/ex00/inc/Zombie.hpp
+++ Module_01/ex00/inc/Zombie.hpp
@@ -6,26 +6,27 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 11:45:13 by passunca          #+#    #+#             */
-/*   Updated: 2024/10/19 11:49:26 by passunca         ###   ########.fr       */
+/*   Updated: 2024/10/19 12:27:31 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ZOMBIE_HPP
 #define ZOMBIE_HPP
 
-#include <string>
+#include <string>	// std::string
+#include <iostream>	// std::cout std::endl
 
 class Zombie
 {
   public:
 	Zombie(void);
+	Zombie(std::string name);
 	~Zombie(void);
 
-	Zombie(std::string name);
 	void announce(void);
 
   private:
-	std::string _name;
+	const std::string _name;
 };
 
 Zombie *newZombie(std::string name);
---
+++ Module_01/ex00/src/Zombie.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/19 12:02:55 by passunca          #+#    #+#             */
+/*   Updated: 2024/10/19 12:27:46 by passunca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Zombie.hpp"
+
+/**
+ * @brief Zombie Class Constructor
+ */
+Zombie::Zombie(void)
+{
+}
+
+/**
+ * @brief Zombie Parameterized Constructor
+ * @param name Zombie's name
+ * @note Member Initializer List:
+ - (https://en.cppreference.com/w/cpp/language/constructor)
+ * - more efficient than assignment;
+ * - If '_name' were a const or reference member list initialization
+ *   would be mandatory;
+ */
+Zombie::Zombie(std::string name) : _name(name)
+{
+}
+
+/**
+ * @brief Zombie Class Destructor
+ */
+Zombie::~Zombie(void)
+{
+	std::cout << "Zombie " << _name << " destroyed ☠" << std::endl;
+}
+
+/* Zombie's Public Methods */
+
+/**
+ * @brief Announce Zombie
+ */
+void Zombie::announce(void)
+{
+	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
