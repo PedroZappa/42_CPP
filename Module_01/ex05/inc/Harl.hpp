c34d01871abfe18ebe6cd94470b3da1a06825748
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-10-22 12:15:28 +0100

    Module_01/ex05/ : basics

---
+++ Module_01/ex05/inc/Harl.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 11:03:13 by passunca          #+#    #+#             */
+/*   Updated: 2024/10/22 11:06:42 by passunca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream> // std::cout
+#include <string>   // std::string
+
+class Harl {
+private:
+  void debug(void);
+  void info(void);
+  void warning(void);
+  void error(void);
+
+public:
+  Harl();
+  ~Harl();
+  void complain(std::string level);
+};
+
+#endif
