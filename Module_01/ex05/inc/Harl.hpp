c49637bdc762cf20c915aa5b561da7265268df7f
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-10-22 12:17:55 +0100

    Module_01/ex05/ : ansi

--- Module_01/ex05/inc/Harl.hpp
+++ Module_01/ex05/inc/Harl.hpp
@@ -6,12 +6,14 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 11:03:13 by passunca          #+#    #+#             */
-/*   Updated: 2024/10/22 11:06:42 by passunca         ###   ########.fr       */
+/*   Updated: 2024/10/22 12:17:00 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HARL_HPP
 # define HARL_HPP
+
+#include "../inc/Ansi.h"
 
 #include <iostream> // std::cout
 #include <string>   // std::string
--- Module_01/ex05/src/Harl.cpp
+++ Module_01/ex05/src/Harl.cpp
@@ -6,31 +6,31 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 11:03:29 by passunca          #+#    #+#             */
-/*   Updated: 2024/10/22 12:01:39 by passunca         ###   ########.fr       */
+/*   Updated: 2024/10/22 12:17:32 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Harl.hpp"
 
 void debug() {
-  std::cout << "I love having extra bacon for my "
+  std::cout << GRN "I love having extra bacon for my "
                "7XL-double-cheese-triple-pickle-special-ketchup burger. I "
-               "really do!\n";
+               "really do!\n" NC;
 }
 
 void info() {
-  std::cout << "I cannot believe adding extra bacon costs more money."
+  std::cout << CYN  "I cannot believe adding extra bacon costs more money."
                "You didn’t put enough bacon in my burger! If you did, I "
-               "wouldn’t be asking for more!\n";
+               "wouldn’t be asking for more!\n" NC;
 }
 
 void warning() {
   std::cout
-	  << "I think I deserve to have some extra bacon for free. I’ve been "
-		 "coming for years whereas you started working here since last month.\n";
+	  << YEL "I think I deserve to have some extra bacon for free. I’ve been "
+		 "coming for years whereas you started working here since last month.\n" NC;
 }
 
 void error() {
   std::cout
-	  << "This is unacceptable! I want to speak to the manager now.\n";
+	  << "This is unacceptable! I want to speak to the manager now.\n" NC;
 }
