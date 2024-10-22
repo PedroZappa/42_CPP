e6eb51f0dd8c33601863e88ee20f46c1d6927484
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-10-22 16:00:20 +0100

    Module_01/ex06/ : update

--- Module_01/ex06/src/main.cpp
+++ Module_01/ex06/src/main.cpp
@@ -11,9 +11,29 @@
 /* ************************************************************************** */
 
 #include "../inc/Harl.hpp"
+#include <cstdlib>
 
-int main(void)
+void errExit(const std::string &msg);
+
+int main(int argc, char **argv)
 {
+	Harl harl;
+	std::string level;
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	if (argc != 2)
+		errExit("Wrong number of arguments");
 	headerPrinter("Harl 2.0 Filter", 50, '-', BRED);
+
 	return (0);
+}
+
+/**
+ * @brief Print error message and exit
+ * @param msg Error message
+*/
+void errExit(const std::string &msg)
+{
+	std::cerr << RED << msg << "\n" NC;
+	exit(EXIT_FAILURE);
 }
