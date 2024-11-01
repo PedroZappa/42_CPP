e1cabf2f5b837e6c991acaae25b583dea61bd1a5
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-11-01 20:13:34 +0000

    Module_01/ex01/ : cleanup

--- Module_01/ex01/src/zombieHorde.cpp
+++ Module_01/ex01/src/zombieHorde.cpp
@@ -50,8 +50,8 @@ Zombie *zombieHorde(int N, std::string name) {
 	// Create zombies
 	for (int i = 0; i < N; i++) {
 		// Generate a random index
-		int randomIndex = rand() % suffixN;
-		std::string currName = name + suffixes[randomIndex];
+		int randomIndex = (rand() % suffixN);
+		std::string currName = (name + suffixes[randomIndex]);
 		horde[i].setName(currName);
 	}
 	return (horde);
