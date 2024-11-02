06aba35b76c10cc5d8d5ca074a52f60f2f1e764a
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-11-02 09:27:53 +0000

    Module_01/ex03/ : cleanup

--- Module_01/ex03/src/main.cpp
+++ Module_01/ex03/src/main.cpp
@@ -16,6 +16,8 @@
 
 int main() {
 	{
+    // Creates an object of type HumanA;
+    // HumanA must have a name and a Weapon
 		Weapon club = Weapon("crude spiked club");
 		HumanA bob("Bob", club);
 		bob.attack();
@@ -23,6 +25,8 @@ int main() {
 		bob.attack();
 	}
 	{
+    // Creates an object of type HumanB;
+    // HumanB only needs a name to be initialized
 		Weapon club = Weapon("crude spiked club");
 		HumanB jim("Jim");
 		jim.setWeapon(club);
