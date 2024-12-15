3aa14950ff80e26b1e0d26deb482315b5a5f3fe9
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-12-15 09:33:26 +0000

    Module_09/ex01 : RPN header plan setup

---
+++ Module_09/ex01/inc/RPN.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 09:27:40 by passunca          #+#    #+#             */
+/*   Updated: 2024/12/15 09:33:03 by passunca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RPN_HPP
+# define RPN_HPP
+
+#include <iostream>
+#include <cstring>
+#include <stack>
+
+class RPN {
+	public:
+	RPN(void);
+	RPN(const RPN &copy);
+	RPN &operator=(const RPN &other);
+	~RPN(void);
+
+	// Public Helpers
+	void parseInput(const std::string elem) const;
+
+private:
+	std::stack<int> _stack;
+
+	// Private Helpers
+	void trimSpaces(std::string &str);
+	bool isStringValid(std::string elem) const;
+	bool calculate(std::string elem);
+	bool doOp(char &op);
+};
+
+#endif
