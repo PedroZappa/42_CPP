349f9ef55be2e864b58d74f941fecf7a7e534e84
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-12-15 10:24:41 +0000

    Module_09/ex01 : string validation

--- Module_09/ex01/src/RPN.cpp
+++ Module_09/ex01/src/RPN.cpp
@@ -6,11 +6,12 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 09:35:04 by passunca          #+#    #+#             */
-/*   Updated: 2024/12/15 10:10:58 by passunca         ###   ########.fr       */
+/*   Updated: 2024/12/15 10:24:05 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/RPN.hpp"
+#include <cstring>
 
 /* ************************************************************************** */
 /*                                Constructors                                */
@@ -37,24 +38,24 @@ RPN::~RPN(void) {
 /* ************************************************************************** */
 
 void RPN::parseInput(std::string input) {
-    trimSpaces(input);
-    if (input.empty()) {
-        std::cerr << RED "Error:" NC " empty input" << std::endl;
-        return ;
-    }
-    if (!isStringValid(input)) {
-        std::cerr << RED "Error:" NC " invalid input" << std::endl;
-        return ;
-    }
-    if (!calculate(input)) {
-        std::cerr << RED "Error:" NC " could not calculate" << std::endl;
-        return ;
-    }
-    if (this->_stack.size() != 1) {
-        std::cerr << RED "Error:" NC " invalid input" << std::endl;
-        return ;
-    }
-    std::cout << _stack.top() << std::endl;
+	trimSpaces(input);
+	if (input.empty()) {
+		std::cerr << RED "Error:" NC " empty input" << std::endl;
+		return;
+	}
+	if (!isStringValid(input)) {
+		std::cerr << RED "Error:" NC " invalid input" << std::endl;
+		return;
+	}
+	if (!calculate(input)) {
+		std::cerr << RED "Error:" NC " could not calculate" << std::endl;
+		return;
+	}
+	if (this->_stack.size() != 1) {
+		std::cerr << RED "Error:" NC " invalid input" << std::endl;
+		return;
+	}
+	std::cout << _stack.top() << std::endl;
 }
 
 /* ************************************************************************** */
@@ -63,7 +64,16 @@ void RPN::parseInput(std::string input) {
 
 void RPN::trimSpaces(std::string &str) {
 	if (str.empty())
-		return ;
+		return;
 	str.erase(str.find_last_not_of(' ') + 1);
 	str.erase(0, str.find_first_not_of(' '));
+}
+
+bool RPN::isStringValid(std::string elem) const {
+	for (size_t i = 0; (i < elem.length()); i++) {
+		char c = elem[i];
+		if (!std::isdigit(c) && !std::strchr("+-*/", c) && !std::isspace(c))
+			return (false);
+	}
+	return (true);
 }
