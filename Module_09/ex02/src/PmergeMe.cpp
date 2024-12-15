fb37d0cdc3034530c4e2c7beed86a5426e10840a
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-12-15 12:06:46 +0000

    Module_09/ex02 : Header planned

---
+++ Module_09/ex02/src/PmergeMe.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 11:56:39 by passunca          #+#    #+#             */
+/*   Updated: 2024/12/15 12:06:01 by passunca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include "../inc/PmergeMe.hpp"
+#include <cctype>
+#include <cstdlib>
+#include <ctime>
+#include <deque>
+#include <exception>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+
+class PmergeMe {
+  public:
+	PmergeMe(void);
+	PmergeMe(const PmergeMe &copy);
+	PmergeMe &operator=(const PmergeMe &other);
+	~PmergeMe(void);
+
+	// Public Functions
+	void parseArgs(int argc, char **argv);
+	void compute(void);
+	double execTime(std::string &container);
+
+	// Templates for Merging
+	template <typename C>
+	void mergeSort(C &container);
+	template <typename C>
+	void merge(C &container);
+
+  private:
+	std::vector<int> _vector;
+	std::deque<int> _deque;
+};
+
+#endif
