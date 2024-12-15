fd14f3a76a01e8061f66ec0151766c09bd61eb89
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-12-15 11:45:47 +0000

    cleanup

--- Module_09/ex01/src/main.cpp
+++ Module_09/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 11:07:06 by passunca          #+#    #+#             */
-/*   Updated: 2024/12/15 11:17:44 by passunca         ###   ########.fr       */
+/*   Updated: 2024/12/15 11:45:36 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
-		std::cerr << RED "Usage: " NC "./RPN <operand1> <operand2> <operator>" << std::endl;
+		std::cerr << RED "Usage: " NC "./RPN <operand1> <operand2> <operator>"
+				  << std::endl;
 		std::cerr << YEL "Example: " NC "./RPN \"21 21 +\"" << std::endl;
 		return (EXIT_FAILURE);
 	}
 
 	RPN rpn;
 	rpn.parseInput(argv[1]);
-	
+
 	return (EXIT_SUCCESS);
 }
