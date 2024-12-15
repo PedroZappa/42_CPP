143febe52d4c243a65346390c56892f99296e7c3
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-12-15 11:19:13 +0000

    Module_09/ex01 : working, needs thorough testinbg

--- Module_09/ex01/src/main.cpp
+++ Module_09/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 11:07:06 by passunca          #+#    #+#             */
-/*   Updated: 2024/12/15 11:09:30 by passunca         ###   ########.fr       */
+/*   Updated: 2024/12/15 11:17:44 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		std::cerr << "Usage: ./RPN <operand1> <operand2> <operator>" << std::endl;
-		std::cerr << "Example: ./RPN 21 21 +" << std::endl;
+		std::cerr << "Example: ./RPN \"21 21 +\"" << std::endl;
 		return (EXIT_FAILURE);
 	}
 
