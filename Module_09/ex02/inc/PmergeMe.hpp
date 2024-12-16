6708072ea04531bfbc0d99597a3d810572a03449
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-12-16 11:23:59 +0000

    Module_09/ tweaks

--- Module_09/ex02/inc/PmergeMe.hpp
+++ Module_09/ex02/inc/PmergeMe.hpp
@@ -10,4 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <cctype>
+#include <cstdlib>
+#include <ctime>
+#include <deque>
+#include <exception>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+
+class PmergeMe {
+  public:
+	PmergeMe(void);
+	PmergeMe(const PmergeMe &copy);
+	PmergeMe &operator=(const PmergeMe &other);
+	~PmergeMe(void);
+
+	// Public Functions
+	void parseArgs(int argc, char **argv);
+	void computeExecTime(std::string &container);
+	double execTime(std::string &container);
+
+	// Templates for Merging
+	template <typename C>
+	void mergeSort(C &container);
+	template <typename C>
+	void merge(C &left, C &right, C &og);
+
+  private:
+	std::vector<int> _vector;
+	std::deque<int> _deque;
+};
+
+#endif
 
