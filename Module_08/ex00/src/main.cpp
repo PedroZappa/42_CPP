5ee5765ef11285fe69aff0172c20c3ad713c073d
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-03-11 09:20:36 +0000

    style(main): reformat code for better readability
    Reformatted the main.cpp file to improve code readability by adjusting line breaks and spacing. This change enhances the clarity of the code structure without altering functionality.
    
    Signed-off-by: Zedro <45104292+PedroZappa@users.noreply.github.com>

--- Module_08/ex00/src/main.cpp
+++ Module_08/ex00/src/main.cpp
@@ -12,28 +12,30 @@
 
 /**
  * @file main.cpp
- * @brief This file contains the main function to demonstrate the usage of the easyfind function.
- * 
- * The program fills a vector and a list with integers and attempts to find specific values using the easyfind function.
- * It demonstrates exception handling when a value is not found.
- * 
+ * @brief This file contains the main function to demonstrate the usage of the
+ * easyfind function.
+ *
+ * The program fills a vector and a list with integers and attempts to find
+ * specific values using the easyfind function. It demonstrates exception
+ * handling when a value is not found.
+ *
  * @author passunca
  * @date 2024-12-14
  */
 
 #include "../inc/easyfind.hpp"
-#include <vector>
 #include <list>
+#include <vector>
 
 #define WIDTH 50
 
 /**
  * @brief Main function to demonstrate the easyfind function.
- * 
+ *
  * This function fills a vector and a list with integers from 0 to 9.
  * It then attempts to find a specific value in each container and prints the result.
  * If the value is not found, an exception is caught and an error message is displayed.
- * 
+ *
  * @return int Returns 0 on successful execution.
  */
 int main(void) {
@@ -52,22 +54,22 @@ int main(void) {
 
 	// Try Vector
 	try {
-		std::cout << "Searching for " BBLU << toSearch << NC " on vector" << std::endl;
+		std::cout << "Searching for " BBLU << toSearch << NC " on vector"
+				  << std::endl;
 		idx = easyfind(vec, toSearch);
 		std::cout << "Value found at index: " YEL << idx << NC << std::endl;
-	}
-	catch (std::exception &e) {
+	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
 	sepPrinter(WIDTH, '-', GRN, 1);
-	
+
 	// Try List
 	try {
-		std::cout << "Searching for " BBLU << outOfRange << NC " on list" << std::endl;
+		std::cout << "Searching for " BBLU << outOfRange << NC " on list"
+				  << std::endl;
 		idx = easyfind(lst, outOfRange);
 		std::cout << "Value found at index: " << YEL << idx << NC << std::endl;
-	}
-	catch (std::exception &e) {
+	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
 	sepPrinter(WIDTH, '-', GRN, 1);
@@ -75,13 +77,12 @@ int main(void) {
 
 /**
  * @brief Exception message for when a value is not found.
- * 
+ *
  * This function overrides the what() method to provide a custom error message
  * when a value is not found in the container.
- * 
+ *
  * @return const char* The error message.
  */
 const char *NotFound::what() const throw() {
 	return (RED "Value not found" NC);
 }
-
